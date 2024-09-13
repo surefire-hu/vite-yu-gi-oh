c3c3fdb6f4d798b890684653a8d470427cbb6258
surefire-hu <surefire.hu2001@gmail.com>
2024-09-13 11:50:24 +0200

    fatto header

---
+++ src/components/general/AppLogo.vue
@@ -0,0 +1,24 @@
+<script>
+    export default {
+        name: 'AppLogo',
+    }
+</script>
+
+<template>
+    <div class="logo">
+        <img src="/public/yu-gi-oh.jpeg" alt="logo Yu-gi-oh">
+    </div>
+</template>
+
+<style lang="scss" scoped> 
+.logo {
+    width: 80px;
+    padding: 20px;
+
+    img {
+        width: 100%;
+        display: block;
+    }
+}
+
+</style>
---
+++ src/components/header/AppHeader.vue
@@ -0,0 +1,30 @@
+<script>
+import AppLogo from '../general/AppLogo.vue';
+
+export default {
+  name:'AppHeader',
+    components: {
+        AppLogo,
+    }
+};
+</script>
+
+<template>
+    <header>
+        <div id="navbar">
+            <AppLogo></AppLogo>
+            <h2>Yu-Gi-Oh Api</h2>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+@use '../../styles/partials/variables' as *;
+@use '../../styles/partials/mixin' as *;
+
+    #navbar {
+        @include flex;
+        background-color: $color-white;
+    } 
+
+</style>
