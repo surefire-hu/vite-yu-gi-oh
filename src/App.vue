b3ab597005d96c5678832e17d7c66b618c94d06d
surefire-hu <166971184+surefire-hu@users.noreply.github.com>
2024-09-13 11:15:57 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script>
+/* 
+  Per importare ed utilizzare un componente dentro un altro devo SEMPRE seguire questi 3 passi:
+  1) Importazione del componente
+  2) Dichiarazione del componente
+  3) Utilizzo del componente
+*/
+// 1) Importazione del componente
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  data() {
+    return { 
+      count: 0
+    }
+  },
+  // 2) Dichiarazione del componente
+  components: {
+    AppHeader
+  },
+  methods: {
+    incrementCount() {
+      this.count++;
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <!-- 3) Utilizzo del componente -->
+    <AppHeader />
+    
+    <main>
+      <button @click="incrementCount()">
+        {{ count }}
+      </button>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+main {
+  text-align: center;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  data() {
+    return { 
+      message: 'Template Vite + Vue'
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+    <h1>
+      {{ message }}
+    </h1>
+  </header>
+</template>
+
+<style scoped>
+header {
+  background-color: aquamarine;
+  text-align: center;
+  padding: 20px 0;
+}
+
+h1 {
+  color: lightcoral;
+}
+</style>
